8bf17c21fc95cec13d6f7efa716d072b11d87716
Manoel Blue <manoelbluee@gmail.com>
2021-02-10 13:15:49 +0100

    add secret variable

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import axios from 'axios';
 
 import './App.css';
-import apiKey from './config';
+import apiKey, {secret} from './config';
 import SearchForm from './components/SearchForm';
 import Nav from './components/Nav';
 import PhotoContainer from './components/PhotoContainer';
@@ -15,7 +15,7 @@ class App extends Component {
 
     componentDidMount() {
         axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=6c1a7b8328a70800df44145bf3795806&safe_search=sunsets&per_page=&format=json&nojsoncallback=1&auth_token=72157718237004357-cb69c906d6263a9c&api_sig=8647980d163903d9c7491b238b8b9c21`)
-            .then(response => console.log(response.data.photos.photo))
+            .then(response => console.log(response.data))
             .catch(error => console.log(error));
     }
 
